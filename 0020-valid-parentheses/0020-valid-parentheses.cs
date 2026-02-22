188e8bb13bb08259466f8cd412be0db6b8c48f7a
Andrew Hany Sobhy <andrewhfarid@gmail.com>
2026-02-23 01:45:16 +0200

    Time: 2 ms (86.38%), Space: 42.2 MB (75.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cs
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -2,7 +2,7 @@ public class Solution {
     public bool IsValid(string s) {
         //loop on every element
         // if opening bracket add to stack if closing pop
-        // if diff kind of bracket
+        // if diff kind of bracket return false
         Stack<char> myStack = new Stack<char>();
         myStack.Push('T'); //push initial character to avoid stack empty exception
         for(int i =0;i<s.Length;i++)
